3c1fd06bfd74b93171772c2d9dbc03d625fa1f75
tpc <1213225994@qq.com>
2020-12-10 16:37:07 +0800

    fix(DTO): PageDTO

---
+++ housekeeping-admin/housekeeping-admin-interface/src/main/java/com/housekeeping/admin/dto/AdminPageDTO.java
@@ -0,0 +1,20 @@
+package com.housekeeping.admin.dto;
+
+import lombok.Data;
+
+@Data
+public class AdminPageDTO {
+
+    private Integer id;
+
+    private String number;
+
+    private String nickname;
+
+    private String name;
+
+    private String phone;
+
+    private String email;
+
+}
