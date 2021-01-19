aeaf165c2a075cfe6a405bb647a6d811cff39d01
tpc <1213225994@qq.com>
2021-01-19 09:24:31 +0800

    feat(控制层): 二维码生成增加头像

--- housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/controller/QrController.java
+++ housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/controller/QrController.java
@@ -1,6 +1,9 @@
 package com.housekeeping.admin.controller;
 
 import com.aliyun.oss.OSSClient;
+import com.aliyun.oss.model.GetObjectRequest;
+import com.aliyun.oss.model.OSSObject;
+import com.aliyun.oss.model.ObjectMetadata;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.WriterException;
@@ -8,9 +11,9 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.housekeeping.admin.dto.EmployeesJobsDTO;
-import com.housekeeping.common.utils.CommonConstants;
-import com.housekeeping.common.utils.R;
-import com.housekeeping.common.utils.TokenUtils;
+import com.housekeeping.admin.service.EmployeesDetailsService;
+import com.housekeeping.admin.service.ManagerDetailsService;
+import com.housekeeping.common.utils.*;
 import com.netflix.ribbon.proxy.annotation.Http;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -23,15 +26,10 @@ import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.image.BufferedImage;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 /**
  * @Author su
@@ -43,6 +41,10 @@ import java.util.Random;
 @RequestMapping("/qr")
 public class QrController {
 
+    @Resource
+    private final EmployeesDetailsService employeesDetailsService;
+    @Resource
+    private final ManagerDetailsService managerDetailsService;
     @Resource
     private OSSClient ossClient;
 
@@ -113,5 +115,60 @@ public class QrController {
         }
         return R.ok(res, "轉換成功，已生成鏈接");
     }
+
+    @ApiOperation("二维码生成，返回链接(type:1为经理 2为员工)")
+    @GetMapping("/generateQr3")
+    public R generateQr3(@RequestParam String content,
+                         @RequestParam Integer typeId,
+                         @RequestParam Integer id) throws IOException {
+        ByteArrayOutputStream out = null;
+        String res = "";
+        try {
+            String destPath = "/Qrcode/id.png";
+            String impPath = "D:/head.jpg";
+            String logoPath = null;
+
+            if(typeId == 1){
+                logoPath = managerDetailsService.getById(id).getHeadUrl();
+            }
+            if(typeId == 2){
+                logoPath = employeesDetailsService.getById(id).getHeadUrl();
+            }
+
+            if(CommonUtils.isEmpty(logoPath)){
+                QrCodeUtils.encode(content,"",destPath,true);
+            }else {
+                String s = logoPath.replaceAll("https://test-live-video.oss-cn-shanghai.aliyuncs.com/", "");
+
+                ossClient.getObject(new GetObjectRequest(bucketName, s), new File(impPath));
+
+                QrCodeUtils.encode(content,impPath,destPath,true);
+
+            }
+
+            File file = new File(destPath);
+            //进行oss存储
+            Random random = new Random();
+            Integer ra = random.nextInt(10000);
+            Integer userId = TokenUtils.getCurrentUserId();
+            LocalDateTime now = LocalDateTime.now();
+            String nowString = now.format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+            String catalogue = CommonConstants.HK_LOGIN_IN_QR_ABSTRACT_PATH_PREFIX_PROV;
+            String type = ".png";
+            String fileAbstractPath = catalogue + "/" + nowString + userId + ra + type;
+            ossClient.putObject(bucketName, fileAbstractPath, file);
+            res = urlPrefix + fileAbstractPath;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (out != null){
+                out.flush();
+                out.close();
+            }
+        }
+        return R.ok(res, "轉換成功，已生成鏈接");
+    }
+
 
 }
