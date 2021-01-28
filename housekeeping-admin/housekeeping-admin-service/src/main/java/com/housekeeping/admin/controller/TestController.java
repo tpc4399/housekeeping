39d88a5e7aa976947ca83c00fbc326d49bd7e786
Suser <1220251182@qq.com>
2021-01-28 11:52:33 +0800

    refactor(接口): 单元测试接口
    地址编码接口

--- housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/controller/TestController.java
+++ housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.housekeeping.admin.controller;
 
+import com.housekeeping.admin.service.IAddressCodingService;
 import com.housekeeping.admin.service.ICurrencyService;
 import com.housekeeping.common.utils.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import net.sf.json.JSONObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,15 +20,27 @@ import java.math.BigDecimal;
 @Api(tags={"【ZZ】单元测试接口"})
 @RestController
 @AllArgsConstructor
-@RequestMapping("/test")
 public class TestController {
 
     private final ICurrencyService currencyService;
+    private final IAddressCodingService addressCodingService;
 
-    @GetMapping
-    @ApiOperation("单元测试调试接口")
-    public R test(){
+    @GetMapping("/test1")
+    @ApiOperation("测试1")
+    public R test1(){
         currencyService.exchangeRateToBigDecimal("TWD", "CNY", BigDecimal.valueOf(100));
+        return R.ok();
+    }
+
+    @GetMapping("/test2")
+    @ApiOperation("测试2")
+    public R test2(){
+        //把地址存為經緯度 湖北省武汉市洪山区茉莉公馆 高雄市苓雅区四维三路2号
+        JSONObject jsonObject = (JSONObject) addressCodingService.addressCoding("高雄市苓雅区四维三路2号").getData();
+        JSONObject result = (JSONObject) jsonObject.get("result");
+        JSONObject location = (JSONObject) result.get("location");
+        Double lng = (Double) location.get("lng");
+        Double lat = (Double) location.get("lat");
         return R.ok();
     }
 
