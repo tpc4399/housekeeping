8874587295d60de6848203f37e01008aa5d5ed07
tpc <1213225994@qq.com>
2021-04-07 10:35:00 +0800

    feat(接口): 服务熔断

--- housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/HousekeepingAdminApplication.java
+++ housekeeping-admin/housekeeping-admin-service/src/main/java/com/housekeeping/admin/HousekeepingAdminApplication.java
@@ -2,6 +2,7 @@ package com.housekeeping.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.core.annotation.Order;
@@ -12,6 +13,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableAsync
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 @EnableFeignClients
 @EnableTransactionManagement
 public class HousekeepingAdminApplication {
