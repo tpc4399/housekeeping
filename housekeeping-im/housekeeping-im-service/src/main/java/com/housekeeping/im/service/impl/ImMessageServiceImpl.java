748b20fccf1a95efb3068031c3aef19151f565c8
tpc <1213225994@qq.com>
2021-05-21 14:39:00 +0800

    feat(接口): 消息列表优化

--- housekeeping-im/housekeeping-im-service/src/main/java/com/housekeeping/im/service/impl/ImMessageServiceImpl.java
+++ housekeeping-im/housekeeping-im-service/src/main/java/com/housekeeping/im/service/impl/ImMessageServiceImpl.java
@@ -42,7 +42,7 @@ public class ImMessageServiceImpl extends ServiceImpl<ImMessageMapper, ImMessage
             return R.ok(null);
         }
         QueryWrapper<ImMessage> wrapper = new QueryWrapper<>();
-        wrapper.orderByDesc("id");
+        /*wrapper.orderByDesc("id");*/
         System.out.println(FRIEND);
         if(FRIEND.equals(chatType)){
             wrapper.and(wrapper1 -> wrapper1.eq("to_id", chatId)
@@ -75,7 +75,7 @@ public class ImMessageServiceImpl extends ServiceImpl<ImMessageMapper, ImMessage
             message.setMsgtype(imMessage.getMsgType());
             messageList.add(message);
         }
-        Page pages = PageUtils.getPages((int)page.getCurrent(), 50, messageList);
+        Page pages = PageUtils.getPages((int)page.getCurrent(), messageList.size(), messageList);
         return R.ok(pages);
     }
 
