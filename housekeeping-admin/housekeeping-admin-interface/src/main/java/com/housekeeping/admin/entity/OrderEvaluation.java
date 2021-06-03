602a2331c118091eb070c4118e5e1d7e61e9624b
Suser <18995636120@163.com>
2021-06-03 17:57:45 +0800

    feat(接口): 雙向評價設計

---
+++ housekeeping-admin/housekeeping-admin-interface/src/main/java/com/housekeeping/admin/entity/OrderEvaluation.java
@@ -0,0 +1,26 @@
+package com.housekeeping.admin.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @Author su
+ * @create 2021/6/3 17:19
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderEvaluation {
+
+    private String orderNumber; //訂單編號
+    /* 客戶評價保潔員 */
+    private Integer starRating1; //評分 1 2 3 4 5
+    private String evaluation1;  //評價
+    private String imageUrls1;   //多張照片urls
+    /* 保潔員評價客戶 */
+    private Integer starRating2; //評分 1 2 3 4 5
+    private String evaluation2;  //評價
+    private String imageUrls2;   //多張照片urls
+
+}
